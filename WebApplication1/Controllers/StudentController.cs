f189eccd8822393bb52c78a3cddd9d56af935516
SauravTandon <SauravTandon@GEDU-LP389>
2024-01-18 00:09:55 +0530

    Add project files.

---
+++ WebApplication1/Controllers/StudentController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.name = "Saurav";
+            ViewBag.address = "Lucknow";
+            return View();
+        }
+        public IActionResult Index2()
+        {
+            Models.Student student = new Models.Student { Name = "Saurav", Address = "Lucknow" };
+            return View(student);
+        }
+    }
+}
